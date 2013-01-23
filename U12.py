0c64169bcc7ad400d28fccf851d3092a57fd10fe
Markus Klemm <markus@markusklemm.net>
2013-01-23 16:57:33 +0100

    Initial

---
+++ U12.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# encoding=utf-8
+
+"""Übung 11 Stratosphärensprung"""
+#Markus Klemm WS12/13 Phy-BA
+
+from __future__ import division
+import matplotlib.pyplot as plt
+import scipy as sp
+import scipy.integrate
+from matplotlib import pyplot as plt
